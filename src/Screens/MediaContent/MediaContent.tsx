284f492739297b5d6b2a2e57c25ba0b404777d07
authmanshimaly <authmanalshimaly71@gmail.com>
2025-08-06 19:16:42 +0300

    fix style

--- src/Screens/MediaContent/MediaContent.tsx
+++ src/Screens/MediaContent/MediaContent.tsx
@@ -6,32 +6,39 @@ import PlaylistTypeModal from "../../Components/Models/PlaylistTypeModal";
 
 const MediaContent = () => {
   const [modalOpen, setModalOpen] = useState(false);
+
   return (
-    <div className="p-6 bg-[var(--white-200)] min-h-screen space-y-8">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-bold text-[var(--black)]">
+    <div className="px-3 sm:px-4 lg:px-6 py-4 bg-[var(--white-200)] min-h-screen space-y-8">
+      {/* Header */}
+      <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-3 sm:gap-4">
+        <h1 className="text-xl sm:text-2xl font-bold text-[var(--black)]">
           Media Content
         </h1>
         <button
           onClick={() => setModalOpen(true)}
-          className="bg-[var(--mainred)] text-white px-4 py-2 rounded-lg flex items-center gap-2 shadow hover:bg-red-600 transition cursor-pointer"
+          className="bg-[var(--mainred)] text-white px-3 py-1.5 rounded-lg flex items-center gap-2 shadow hover:bg-red-600 transition text-sm"
         >
-          <Plus size={18} />
-          Add New Playlist
+          <Plus size={16} />
+          <span>Add New Playlist</span>
         </button>
       </div>
+
+      {/* Normal Playlist */}
       <div>
-        <h2 className="text-2xl font-semibold text-[var(--black)] py-5">
+        <h2 className="text-lg sm:text-xl font-semibold text-[var(--black)] py-3 sm:py-4">
           Normal Playlist
         </h2>
         <MediaCard />
       </div>
+
+      {/* Interactive Playlist */}
       <div>
-        <h2 className="text-2xl font-semibold text-[var(--black)] py-5">
+        <h2 className="text-lg sm:text-xl font-semibold text-[var(--black)] py-3 sm:py-4">
           Interactive Playlist
         </h2>
         <MediaCard />
       </div>
+
       {/* Playlist Type Modal */}
       <BaseModal
         open={modalOpen}
