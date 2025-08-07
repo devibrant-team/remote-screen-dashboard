6980ce368a6879bf0671022544438d4e94b0b5fa
QusayS17 <kosaysolh0@gmail.com>
2025-08-07 18:28:31 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import ToolBar from './layout/Tabbar';
 import MediaContent from './Screens/MediaContent/MediaContent';
 import PlayList from './Screens/Playlist/Normal/PlayList';
@@ -7,25 +7,38 @@ import LoginScreen from './Screens/AuthScreens/LoginScreen';
 import LicenseKey from './../LicenseKey/LicenseKey';
 import Test from './Screens/Test';
 
+function AppWrapper() {
+  const location = useLocation();
+
+  // Pages where ToolBar should NOT be shown
+  const hideToolbarPaths = ['/login', '/license' , '/playlist'];
+
+  const shouldHideToolbar = hideToolbarPaths.includes(location.pathname);
+
+  return (
+    <div className="flex">
+      {/* Conditionally render ToolBar */}
+      {!shouldHideToolbar && <ToolBar />}
+
+      <div className="flex-1 bg-[var(--white-200)] p-4 w-full">
+        <Routes>
+          <Route path="/mediacontent" element={<MediaContent />} />
+          <Route path="/playlist" element={<PlayList />} />
+          <Route path="/interactive" element={<CreateInteractivePlaylist />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/license" element={<LicenseKey />} />
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+// Wrap AppWrapper in Router
 function App() {
   return (
     <Router>
-      <div className="flex">
-        {/* Sidebar always visible */}
-        <ToolBar />
-
-        {/* Main content area */}
-        <div className="flex-1 bg-[var(--white-200)]  p-4 w-full">
-          <Routes>
-            <Route path="/mediacontent" element={<MediaContent />} />
-            <Route path="/playlist" element={<PlayList />} />
-            <Route path="/interactive" element={<CreateInteractivePlaylist />} />
-            <Route path="/login" element={<LoginScreen />} />
-            <Route path="/license" element={<LicenseKey />} />
-            <Route path="/test" element={<Test />} />
-          </Routes>
-        </div>
-      </div>
+      <AppWrapper />
     </Router>
   );
 }
