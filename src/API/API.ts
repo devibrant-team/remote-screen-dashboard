4c8afb24a20ec0d3c2028f7288689376ca25ba38
authmanshimaly <authmanalshimaly71@gmail.com>
2025-08-07 20:24:29 +0300

    post of interactive

--- src/API/API.ts
+++ src/API/API.ts
@@ -1,4 +1,5 @@
 export const http =  "http://192.168.10.107/remote-screen-backend/public/api/";
 export const loginApi = `${http}dashboard/login`
 export const playlisttypeApi = `${http}getplaylistStyle`
+export const postplaylistinteractiveApi = `${http}postinteractive`
 
