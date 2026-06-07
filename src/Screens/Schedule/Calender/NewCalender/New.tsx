1901d48962152d30578eb4484a86846e7ceb587d
Qusay Solh <kosaysolh0@gmail.com>
2026-06-07 22:00:23 +0300

    refactor: clean up layout structure and improve code readability in New component

--- src/Screens/Schedule/Calender/NewCalender/New.tsx
+++ src/Screens/Schedule/Calender/NewCalender/New.tsx
@@ -4,24 +4,22 @@ import ReservedScheduleToolbar from "./ReservedScheduleToolbar";
 const New = () => {
   return (
     <div className="flex h-screen w-full bg-[#f8f9fa] text-[#1a1f2e] overflow-hidden">
-      {/* ===== LEFT TOOLBAR (desktop persistent, sticky) ===== */}
       <aside
         className="
-    hidden lg:flex w-[330px] shrink-0 border-r border-gray-200 bg-white
-    lg:sticky lg:top-0 lg:h-screen lg:overflow-y-auto
-    /* hide the scrollbar cross-browser */
-    lg:[scrollbar-width:none]                 /* Firefox */
-    lg:[-ms-overflow-style:none]              /* old IE/Edge */
-    lg:[&::-webkit-scrollbar]:hidden          /* Chrome/Safari/Edge */
-    px-4 py-4
-  "
+          hidden lg:flex w-[330px] shrink-0 border-r border-gray-200 bg-white
+          lg:sticky lg:top-0 lg:h-screen lg:overflow-y-auto
+          lg:[scrollbar-width:none]lg:[scrollbar-width:none]             lg:[-ms-overflow-style:none]
+ lg:[-ms-overflow-style:none]              lg:[&::-webkit-scrollbar]:hiddenlg:[&::-webkit-scrollbar]:hidden          px-4 py-4
+        "
       >
         <div className="flex h-full w-full flex-col">
-<ReservedScheduleToolbar/>
+          <ReservedScheduleToolbar />
         </div>
       </aside>
 
-      <NewCalender />
+      <main className="min-w-0 flex-1 h-screen overflow-y-auto p-4">
+        <NewCalender />
+      </main>
     </div>
   );
 };
